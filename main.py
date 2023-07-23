1b6acb1b229289e8e3d66266606c4fe8896259dd
GoldenKnight123 <hanxiangw0599@gmail.com>
2023-07-23 18:54:23 +1200

    Third commit
    Added checkboxes and displayboxes. Created the layout of the program with no decoration.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class MainHandler:
         self.screen = screen
         self.running = True
         self.clock = pygame.time.Clock()
-        self.fps = 60
+        self.fps = 240
         self.objects = []
 
     def update(self):
@@ -31,7 +31,7 @@ class MainHandler:
         
         self.screen.events(events)
         for i in self.objects:
-            i.events(events)
+            i.events(events) #pass events to all objects to save a large amonut of computing power
 
     def run(self):
         while self.running:
@@ -40,7 +40,7 @@ class MainHandler:
             self.update()
             self.draw()
 
-    def addObject(self, obj):
+    def addObject(self, obj): #Not really nessecary but makes code look nicer
         self.objects.append(obj)
 
 #This class is the screen of the program
@@ -49,7 +49,7 @@ class MainHandler:
 class MainScreen:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((640, 480))
+        self.screen = pygame.display.set_mode((1280, 720))
         self.logo = pygame.image.load("logo.png")
         self.logo = pygame.transform.scale(self.logo, (100, 100))
         pygame.display.set_caption("NCEA AI")
@@ -68,14 +68,47 @@ class MainScreen:
         self.screen.blit(text, (115, 10 ))
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': #Only run when ran as a script
     screen = MainScreen()
     app = MainHandler(screen)
-    test_button = BasicButton((100, 50), (100, 100), "Test", pygame.font.Font("Kanit-Regular.ttf", 24), (0, 0, 255), (0, 0, 170), None)
-    app.addObject(test_button)
-    test_dropbox = DropDown((100, 50), (100, 100), ["Test", "Test2", "Test3"], pygame.font.Font("Kanit-Regular.ttf", 24), (255, 255, 255), (192, 192, 192), (100, 100, 100))
-    test_editbox = EditBox((100, 50), (100, 200), "Test", pygame.font.Font("Kanit-Regular.ttf", 24), (255, 255, 255), (192, 192, 192), (100, 100, 100), (0, 0, 0), 10)
-    app.addObject(test_dropbox)
-    app.addObject(test_editbox)
+    #test_button = BasicButton((100, 50), (100, 100), "Test", pygame.font.Font("Kanit-Regular.ttf", 24), (0, 0, 255), (0, 0, 170), None)
+    #app.addObject(test_button)
+    subject_dropbox = DropDown((145, 30), (15, 110), ["Choose Subject...", "Physics", "Chemistry"], pygame.font.Font("Kanit-Regular.ttf", 12), (255, 255, 255), (192, 192, 192), (100, 100, 100))
+    level_dropbox = DropDown((145, 30), (15, 150), ["Choose Level...", "Level 1", "Level 2", "Level 3"], pygame.font.Font("Kanit-Regular.ttf", 12), (255, 255, 255), (192, 192, 192), (100, 100, 100))
+    topic_editbox = EditBox((145, 30), (15, 190), "Enter Topic...", pygame.font.Font("Kanit-Regular.ttf", 12), (255, 255, 255), (192, 192, 192), (100, 100, 100), (0, 0, 0), 25)
+    diff_dropbox = DropDown((145, 30), (15, 230), ["Choose Difficulty...", "Achieved", "Merit", "Excellence"], pygame.font.Font("Kanit-Regular.ttf", 12), (255, 255, 255), (192, 192, 192), (100, 100, 100))
+    text1 = SimpleText((15, 270), "Generative Options:", pygame.font.Font("Kanit-Regular.ttf", 16), (0, 0, 0))
+    question_checkbox = CheckBox((30, 30), (15, 300), pygame.font.Font("Kanit-Regular.ttf", 12), (255, 255, 255), (192, 192, 192), (100, 100, 100), (0, 0, 0), "Question", False)
+    answer_checkbox = CheckBox((30, 30), (15, 340), pygame.font.Font("Kanit-Regular.ttf", 12), (255, 255, 255), (192, 192, 192), (100, 100, 100), (0, 0, 0), "Answer", False)
+    explanation_checkbox = CheckBox((30, 30), (15, 380), pygame.font.Font("Kanit-Regular.ttf", 12), (255, 255, 255), (192, 192, 192), (100, 100, 100), (0, 0, 0), "Explanation", False)
+    text2 = SimpleText((600, 10), "Generations", pygame.font.Font("Kanit-Regular.ttf", 48), (0, 0, 0))
+    text3 = SimpleText((600, 70), "Question:", pygame.font.Font("Kanit-Regular.ttf", 24), (0, 0, 0))
+    question_displaybox = DisplayBox((500, 250), (600, 110), pygame.font.Font("Kanit-Regular.ttf", 12), (255, 255, 255), (192, 192, 192), (0, 0, 0), """Question goes here...""")
+    text4 = SimpleText((600, 360), "Answer/Explanation::", pygame.font.Font("Kanit-Regular.ttf", 24), (0, 0, 0))
+    answer_displaybox = DisplayBox((500, 250), (600, 400), pygame.font.Font("Kanit-Regular.ttf", 12), (255, 255, 255), (192, 192, 192), (0, 0, 0), "Answer goes here...")
+    generate_button = BasicButton((100, 50), (15, 500), "Generate", pygame.font.Font("Kanit-Regular.ttf", 20), (192, 192, 192), (100, 100, 100), None)
+    save_button = BasicButton((100, 50), (15, 560), "Save", pygame.font.Font("Kanit-Regular.ttf", 20), (192, 192, 192), (100, 100, 100), None)
+    
+    #Add in reverse order to prevent boxes from being drawn over each other
+    
+    #Input Side
+    app.addObject(save_button)
+    app.addObject(generate_button)
+    app.addObject(answer_checkbox)
+    app.addObject(question_checkbox)
+    app.addObject(text1)
+    app.addObject(diff_dropbox)
+    app.addObject(topic_editbox)
+    app.addObject(level_dropbox)
+    app.addObject(subject_dropbox)
+
+    #Generation Side
+    app.addObject(answer_displaybox)
+    app.addObject(question_displaybox)
+    app.addObject(text2)
+    app.addObject(text3)
+    app.addObject(text4)
+    app.addObject(explanation_checkbox)
+
     app.run()
     pygame.quit()
--- utilities.py
+++ utilities.py
@@ -125,11 +125,24 @@ class DropDown:
                 text_rect.centery = self.pos[1] + (i + 1) * self.size[1] + self.size[1] / 2
                 screen.blit(text, text_rect)
 
-        #Built in arrow on the right
-        pygame.draw.polygon(screen, (0, 0, 0), ((self.pos[0] + self.size[0] - 20, self.pos[1] + 20), (self.pos[0] + self.size[0] - 10, self.pos[1] + 20), (self.pos[0] + self.size[0] - 15, self.pos[1] + 30)))
+        #Built in arrow on the right pointing down that scales with the size of the dropdown
+        pygame.draw.polygon(screen, (0, 0, 0), ((self.pos[0] + self.size[0] - self.size[0]/7.5, self.pos[1] + self.size[1]*0.4), (self.pos[0] + self.size[0] - self.size[0]/15, self.pos[1] + self.size[1]*0.4), (self.pos[0] + self.size[0] - self.size[0]/10, self.pos[1] + self.size[1]*0.4 + self.size[1]/5)))
 
     def events(self, events):
         pass
+
+class AdaptiveDropDown(DropDown):
+    def __init__(self, size, pos, options_list, font, colour, border_color, hover_colour, condition):
+        super().__init__(size, pos, options_list[0], font, colour, border_color, hover_colour, condition)
+        options_list = options_list
+        condition = condition
+    
+    def update(self):
+        super().update(self)
+        
+    def draw(self, screen):
+        super().draw(self)
+
 
 #Editbox class
 class EditBox:
@@ -227,3 +240,196 @@ class EditBox:
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_BACKSPACE:
                         self.backspace_hold_timer = 0
+
+#Checkbox class
+class CheckBox:
+    def __init__(self, size, pos, font, colour, border_colour, hover_colour, text_colour, text, checked):
+        self.size = size
+        self.pos = pos
+        self.font = font
+        self.colour = colour
+        self.border_colour = border_colour
+        self.hover_colour = hover_colour
+        self.text_colour = text_colour
+        self.text = text
+        self.checked = checked
+        self.hovered = False
+        self.clicked = False
+
+    def update(self):
+        mouse_pos = pygame.mouse.get_pos()
+        #Check if mouse is hovering over checkbox
+        if self.pos[0] < mouse_pos[0] < self.pos[0] + self.size[0] and self.pos[1] < mouse_pos[1] < self.pos[1] + self.size[1]:
+            self.hovered = True
+            #Check if mouse is clicking on checkbox
+            if pygame.mouse.get_pressed()[0] and not self.clicked:
+                self.clicked = True
+                self.checked = not self.checked
+        else:
+            self.hovered = False
+
+        if not pygame.mouse.get_pressed()[0]:
+            self.clicked = False
+
+    def draw(self, screen):
+        #Draw different colour if hovered
+        if self.hovered:
+            pygame.draw.rect(screen, self.hover_colour, (self.pos[0], self.pos[1], self.size[0], self.size[1]))
+            pygame.draw.rect(screen, self.border_colour, (self.pos[0], self.pos[1], self.size[0], self.size[1]), 2)
+        else:
+            pygame.draw.rect(screen, self.colour, (self.pos[0], self.pos[1], self.size[0], self.size[1]))
+            pygame.draw.rect(screen, self.border_colour, (self.pos[0], self.pos[1], self.size[0], self.size[1]), 2)
+
+        #Draw a check if checked, consists of 2 lines
+        if self.checked:
+            pygame.draw.line(screen, self.text_colour, (self.pos[0] + self.size[0]/4, self.pos[1] + self.size[1]/2), (self.pos[0] + self.size[0]/3, self.pos[1] + self.size[1]*0.75), 3)
+            pygame.draw.line(screen, self.text_colour, (self.pos[0] + self.size[0]/3, self.pos[1] + self.size[1]*0.75), (self.pos[0] + self.size[0]*0.75, self.pos[1] + self.size[1]/4), 3)
+
+        text = self.font.render(self.text, True, self.text_colour)
+        text_rect = text.get_rect()
+        text_rect.left = self.pos[0] + self.size[0] + 10
+        text_rect.centery = self.pos[1] + self.size[1] / 2
+        screen.blit(text, text_rect)
+    
+    def events(self, events):
+        pass
+
+#Static text display class
+class SimpleText:
+    def __init__(self, pos, text, font, colour):
+        self.pos = pos
+        self.text = text
+        self.font = font
+        self.colour = colour
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        text = self.font.render(self.text, True, self.colour)
+        text_rect = text.get_rect()
+        text_rect.left = self.pos[0]
+        text_rect.top = self.pos[1]
+
+        screen.blit(text, text_rect)
+
+    def events(self, events):
+        pass
+
+#Large text display box class
+class DisplayBox:
+    def __init__(self, size, pos, font, colour, border_colour, text_colour, text):
+        self.size = size
+        self.pos = pos
+        self.font = font
+        self.colour = colour
+        self.border_colour = border_colour
+        #No hover color as it is not interactable and the user can only scroll through it
+        self.text_colour = text_colour
+        self.text = []
+        temp_word = ""
+        temp_text = ""
+        for i in range(len(text)):
+            print(self.text)
+            if text[i] == " ":
+                if self.font.size(temp_text + temp_word + " ")[0] >= self.size[0] - 20:
+                    self.text.append(temp_text)
+                    temp_text = temp_word + " "
+                    temp_word = ""
+                else:
+                    temp_text += temp_word + " "
+                    temp_word = ""
+            if text[i] == "\n":
+                if self.font.size(temp_text + temp_word)[0] >= self.size[0] - 20:
+                    self.text.append(temp_text)
+                    temp_text = ""
+                    temp_word = ""
+                else:
+                    self.text.append(temp_text + temp_word)
+                    temp_text = "" 
+                    temp_word = ""
+
+            else:
+                temp_word += text[i]
+        temp_text += temp_word + " "
+        self.text.append(temp_text)
+        self.clicked = False
+        self.scroll_lock = False
+        self.scroll = 0
+        #If the length of the text is greater than the size of the box, the user can scroll through it, else the scroll bar is disabled and not drawn
+        #The max scroll value is the length of the text minus the size of the box
+        if len(self.text) * self.font.size(self.text[0])[1] > self.size[1]:
+            self.max_scroll = (len(self.text) + 1) * self.font.size(self.text[0])[1] - self.size[1]
+        else:
+            self.max_scroll = -1
+
+    def update(self):
+        #If mouse is hovered over scroll bar, the user can slide it up and down to control the scroll
+        mouse_pos = pygame.mouse.get_pos()
+        if self.pos[0] + self.size[0] - 20 < mouse_pos[0] < self.pos[0] + self.size[0] + 20 and self.pos[1] - 20 < mouse_pos[1] < self.pos[1] + self.size[1] + 20:
+            if pygame.mouse.get_pressed()[0] and not self.clicked:
+                self.scroll_lock = True
+                self.scroll = (mouse_pos[1] - self.pos[1] - 6) / (self.size[1] - 14) * self.max_scroll
+
+        if not pygame.mouse.get_pressed()[0]:
+            self.clicked = False
+            self.scroll_lock = False
+
+        if pygame.mouse.get_pressed()[0]:
+            self.clicked = True
+
+        if self.scroll_lock:
+            self.scroll = (mouse_pos[1] - self.pos[1] - 6) / (self.size[1] - 14) * self.max_scroll
+        
+        if self.scroll < 0:
+            self.scroll = 0
+        if self.scroll > self.max_scroll:
+            self.scroll = self.max_scroll
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.colour, (self.pos[0], self.pos[1], self.size[0], self.size[1]))
+        #If text is outside of the box, it isn't drawn
+        #If it is partially outside of the box, it is cut off
+        for i in range(len(self.text)):
+            top = self.pos[1] + (i + 1) * self.font.size(self.text[i])[1] - self.scroll - 10
+            if top > self.pos[1] - self.font.size(self.text[i])[1] and top < self.pos[1] + self.size[1]:
+                text = self.font.render(self.text[i], True, self.text_colour)
+                text_rect = text.get_rect()
+                text_rect.left = self.pos[0] + 10
+                text_rect.top = top
+
+                screen.blit(text, text_rect)
+                #pygame.draw.rect(screen, (255, 0, 0), (text_rect.left, text_rect.top, text_rect.width, text_rect.height), 2)
+
+            elif top < self.pos[1] - self.font.size(self.text[i])[1] and top + self.font.size(self.text[i])[1] > self.pos[1] - self.font.size(self.text[i])[1]:
+                text = self.font.render(self.text[i], True, self.text_colour)
+                text_rect = text.get_rect()
+                text_rect.left = self.pos[0] + 10
+                text_rect.top = top
+
+                screen.blit(text, text_rect)
+                #pygame.draw.rect(screen, (255, 0, 0), (text_rect.left, text_rect.top, text_rect.width, text_rect.height), 2)
+
+            elif top > self.pos[1] + self.size[1] + self.font.size(self.text[i])[1] and top + self.font.size(self.text[i])[1] < self.pos[1] + self.size[1]:
+                text = self.font.render(self.text[i], True, self.text_colour)
+                text_rect = text.get_rect()
+                text_rect.left = self.pos[0] + 10
+                text_rect.top = top
+
+                screen.blit(text, text_rect)
+                #pygame.draw.rect(screen, (255, 0, 0), (text_rect.left, text_rect.top, text_rect.width, text_rect.height), 2)
+
+        pygame.draw.rect(screen, (255, 255, 255), (self.pos[0], self.pos[1]-30, self.size[0], 30))
+        pygame.draw.rect(screen, (255, 255, 255), (self.pos[0], self.pos[1]+self.size[1], self.size[0], 30))
+        pygame.draw.rect(screen, self.border_colour, (self.pos[0], self.pos[1], self.size[0], self.size[1]), 2)
+
+        #draw a scroll bar on the side which is a gray dot that moves if scrolling is needed
+        if self.max_scroll != -1:
+            pygame.draw.circle(screen, (192, 192, 192), (self.pos[0] + self.size[0] - 6, self.pos[1] + 6 + (self.size[1]-11)*self.scroll/self.max_scroll), 5)
+
+    def events(self, events):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.pos[0] < mouse_pos[0] < self.pos[0] + self.size[0] and self.pos[1] < mouse_pos[1] < self.pos[1] + self.size[1]:
+            for event in events:
+                if event.type == pygame.MOUSEWHEEL:
+                    self.scroll -= event.y * 10
